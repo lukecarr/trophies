ef465e15f84bc3cb3dcbbd279ddc347620ec1320
Luke Carr <me+oss@carr.sh>
2023-09-02 16:37:22 +0000

    ♻️ Refactored `tailwind.config.js` to `tailwind.config.ts`

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
     content: [
         "./index.html",
@@ -12,5 +13,4 @@ export default {
         },
     },
     plugins: [],
-}
-
+} satisfies Config
